3c34de799762aab228cad358bb094f0b5e6b6e59
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-30 09:45:52 -0500

    error mesage: does not think correct voice is installed but it it

---
+++ scripts/jp.ps1
@@ -0,0 +1,19 @@
+#get args
+
+
+$word = $args[0]
+
+
+write-host $word
+
+#make tts and save to wav
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoice('Microsoft Hedda Desktop')
+#$speak | Get-Member
+
+
+$speak.SetOutputToWaveFile("../audio-jp/$word.wav")
+$speak.Speak($word)
+$speak.Dispose()
+
