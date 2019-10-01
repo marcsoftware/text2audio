3070ada4f81482d9c8f9e64156dae8aaeb33e620
Marc Melcher <marcdavidmelcher@gmail.com>
2019-10-01 13:20:26 -0500

    added code to print installed voiced, since not all voice listed as intalled in the WINDOWS SETTING PAGE

--- scripts/jp.ps1
+++ scripts/jp.ps1
@@ -6,14 +6,26 @@ $word = $args[0]
 
 write-host $word
 
-#make tts and save to wav
-Add-Type -AssemblyName System.speech
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.SelectVoice('Microsoft Hedda Desktop')
-#$speak | Get-Member
+#make tts and save to wav...
+
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+    # uncomment this if you get 'voice not found error message'
+    # print the installed voices
+    #Echo 'list of installed voices:'
+    #foreach ($voice in $speak.GetInstalledVoices()) {
+    #    Echo $voice.VoiceInfo.Name;
+    #}
+
+    $speak.SelectVoice('Microsoft Haruka Desktop')
 
 
-$speak.SetOutputToWaveFile("../audio-jp/$word.wav")
-$speak.Speak($word)
-$speak.Dispose()
+
+    #$speak | Get-Member
+
+
+    $speak.SetOutputToWaveFile("../audio-jp/$word.wav")
+    $speak.Speak($word)
+    $speak.Dispose()
 
